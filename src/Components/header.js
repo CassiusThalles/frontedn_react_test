95d205b47875ad9dba3bad27c8e735ca13b13e40
cassius <cassiustcmendes@hotmail.com>
2020-02-16 22:59:43 -0300

    continuing project

--- src/Components/header.js
+++ src/Components/header.js
@@ -6,8 +6,18 @@ class Header extends Component{
         return (
             <div className="headerDiv">
                 <header>
-                    <span className="facepayHeader">FacePay</span>
+                    <span className="facepayHeader"><b>Face</b><b style={{"font-weight": "100"}}>Pay</b></span>
+                    <span className="inicio"><a href="/">Início</a></span>
+                    <span className="pagamento"><a href="/">Pagamento</a></span>
+                    <span className="carteiraDigital"><a href="/">Carteira digital</a></span>
+                    <span className="sobreNos"><a href="/">Sobre nós</a></span>
+                    <span className="cadastroHeader">Cadastre-se</span>
                 </header>
+                <div className="imageHeader">
+                    <div className="textOnImage"></div>
+                    <div className="textOnImageH1">Pagar nunca foi tão simpes!</div>
+                    <div className="textOnImageH3">Evite filas para pagar. Com FacePay você paga com o reconhecimento facial, sem precisar de um intermédio físico de pagamento! </div>
+                </div>'
             </div>
         )
     }
