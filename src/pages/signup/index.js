6fad941dbc4444409930f64af18959202b17ed50
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2020-02-17 23:51:41 -0300

    edit page signup

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,11 +1,86 @@
 import React from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Image,
+  InputGroup,
+  FormControl,
+  ButtonToolbar,
+  Button
+} from "react-bootstrap";
 
-import { Container } from "./styles";
+import {
+  Title,
+  ContainerImg,
+  TitleFacePay,
+  PhotoCard,
+  ContainerCard
+} from "./styles";
+
+import ImageSignUp from "../../images/image9.png";
 
 function Signup() {
   return (
-    <Container>
-      <p>oi</p>
+    <Container style={{ margin: 0, maxWidth: "100%" }}>
+      <Row style={{ flex: 1 }}>
+        <Col xl={5} style={{ margin: 0, padding: 0 }}>
+          <ContainerImg>
+            <Image src={ImageSignUp} />
+            <TitleFacePay text1="Face" text2="Pay" />
+          </ContainerImg>
+        </Col>
+        <Col xl={7}>
+          <ContainerCard>
+            <Title>Dados pessoais</Title>
+            <InputGroup className="mb-3">
+              <FormControl placeholder="Nome" />
+              <FormControl placeholder="Sobrenome" />
+            </InputGroup>
+            <InputGroup className="mb-3">
+              <FormControl placeholder="Data nascimento" />
+              <FormControl placeholder="CPF" />
+            </InputGroup>
+            <PhotoCard text="Envie a foto do seu documento" />
+          </ContainerCard>
+          <ContainerCard>
+            <Title>Endereço</Title>
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <FormControl
+                  id="basic-url"
+                  aria-describedby="basic-addon3"
+                  placeholder="CEP"
+                />
+              </InputGroup.Prepend>
+              <InputGroup.Text id="basic-addon3">
+                Não sabe seu CEP?
+              </InputGroup.Text>
+            </InputGroup>
+
+            <InputGroup className="mb-3">
+              <FormControl
+                id="basic-url"
+                aria-describedby="basic-addon3"
+                placeholder="Endereço"
+              />
+            </InputGroup>
+
+            <InputGroup className="mb-3">
+              <FormControl placeholder="Numero" />
+              <FormControl placeholder="Complemento" />
+              <FormControl placeholder="Bairro" />
+            </InputGroup>
+            <PhotoCard text="Envie a foto do seu comprovante de residência" />
+
+            <ButtonToolbar>
+              <Button variant="primary" size="lg" active>
+                Próximo
+              </Button>
+            </ButtonToolbar>
+          </ContainerCard>
+        </Col>
+      </Row>
     </Container>
   );
 }
--- src/pages/signup/styles.js
+++ src/pages/signup/styles.js
@@ -1,7 +1,71 @@
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import Logo from "../../images/PhotoUpload.svg";
 
-export const Container = styled.div`
+
+export const ContainerImg = styled.div`
   display: flex;
-  flex: 1;
-  background: #f63;
+  background: #ff6;
 `;
+export const ContainerCard = styled.div`
+  margin: 40px 0px;
+`;
+export const Title = styled.p`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 30px;
+  line-height: 35px;
+  color: #3c91e6;
+`;
+export function TitleFacePay({ text1, text2 }) {
+  let Container = styled.div`
+    position: absolute;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  `;
+  let Title = styled.p`
+    color: #fff;
+    font-size: 36px;
+    font-weight: bold;
+  `;
+  let Subtitle = styled.p`
+    font-size: 32px;
+    color: #fff;
+  `;
+  return (
+    <Container>
+      <Title>{text1}</Title>
+      <Subtitle>{text2}</Subtitle>
+    </Container>
+  );
+}
+
+export function PhotoCard({ text }) {
+  let Container = styled.div`
+    margin: 30px 0px;
+
+    border-radius: 6px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    background: #fa824c;
+  `;
+
+  let Title = styled.p`
+    padding: 10px 10px;
+    color: #fff;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+  `;
+
+  return (
+    <Container>
+      <img src={Logo} className="App-logo" alt="logo" />
+      <Title>{text}</Title>
+    </Container>
+  );
+}
